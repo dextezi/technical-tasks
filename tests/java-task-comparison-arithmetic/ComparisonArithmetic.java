c0d7d29bf826f619b53fdf1226130ce8b9a7ea2b
dextezi <standaartlol@gmail.com>
2026-09-09 14:26:04 +0400

    edit files

--- tests/java-task-comparison-arithmetic/ComparisonArithmetic.java
+++ tests/java-task-comparison-arithmetic/ComparisonArithmetic.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class ComparisonArithmetic {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
