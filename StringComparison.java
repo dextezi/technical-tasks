cb920faa94f35ad07253e6fbcf1effedb570719b
dextezi <60966766+dextezi@users.noreply.github.com>
2026-09-09 15:04:34 +0400

    add task3 file

---
+++ StringComparison.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class StringComparison {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите строку a: ");
+        String a = scanner.nextLine();
+
+        System.out.print("Введите строку b: ");
+        String b = scanner.nextLine();
+
+        if (a.equals(b)) {
+            System.out.println("Строки идентичны");
+        } else {
+            System.out.println("Строки неидентичны");
+        }
+
+        scanner.close();
+    }
+}
